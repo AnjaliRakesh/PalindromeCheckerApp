83091225f1d461c09b297d07b386af97057b84a9
Anjali <ar7361@srmist.edu.in>
2026-02-25 09:20:07 +0530

    Fix UC3 and UC4 logic inside main method

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -9,24 +9,47 @@ public class PalindromeCheckerApp {
 
         // UC2: Hardcoded Palindrome Check
         String word = "madam";
-
         if (word.equals("madam")) {
-            System.out.println(word + " is a Palindrome");
+            System.out.println(word + " is a Palindrome (UC2)");
         } else {
-            System.out.println(word + " is NOT a Palindrome");
+            System.out.println(word + " is NOT a Palindrome (UC2)");
         }
-    }
-    // UC3: Palindrome using String Reverse
-    String word = "madam";
-    String reverse = "";
 
-for (int i = word.length() - 1; i >= 0; i--) {
-        reverse = reverse + word.charAt(i);
-    }
+        // UC3: Palindrome using String Reverse
+        String word3 = "madam";
+        String reverse = "";
 
-if (word.equals(reverse)) {
-        System.out.println(word + " is a Palindrome (UC3)");
-    } else {
-        System.out.println(word + " is NOT a Palindrome (UC3)");
+        for (int i = word3.length() - 1; i >= 0; i--) {
+            reverse = reverse + word3.charAt(i);
+        }
+
+        if (word3.equals(reverse)) {
+            System.out.println(word3 + " is a Palindrome (UC3)");
+        } else {
+            System.out.println(word3 + " is NOT a Palindrome (UC3)");
+        }
+
+        // UC4: Palindrome using Character Array and Two Pointers
+        String word4 = "madam";
+        char[] chars = word4.toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word4 + " is a Palindrome (UC4)");
+        } else {
+            System.out.println(word4 + " is NOT a Palindrome (UC4)");
+        }
     }
 }
